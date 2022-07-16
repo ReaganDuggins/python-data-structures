e9e433de64e602c91ea41917491097a1e16c3eef
Reagan <7460851+ReaganDuggins@users.noreply.github.com>
2022-07-16 04:52:58 -0500

    [feature] create basic project structure

---
+++ data_structures/linked_list/double_link_list.py
@@ -0,0 +1,5 @@
+class DoubleLinkList:
+    def __init__(self):
+        print('yo')
+
+DoubleLinkList()
---
+++ tests/double_link_list_tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+class DoubleLinkListTests(unittest.TestCase):
+    def test_thing(self):
+        self.assertEqual(4,4)
+
+if __name__ == '__main__':
+    unittest.main()
