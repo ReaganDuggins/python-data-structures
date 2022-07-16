598ad729ad0d6be21163d288ca27be0fc9c0900d
Reagan <7460851+ReaganDuggins@users.noreply.github.com>
2022-07-16 05:38:18 -0500

    [feature] can now add first node to the list

--- data_structures/linked_list/DoubleLinkList.py
+++ data_structures/linked_list/DoubleLinkList.py
@@ -1,3 +1,6 @@
 class DoubleLinkList:
     def __init__(self, head = None):
         self.head = head
+
+    def add(self, new_node):
+        self.head = new_node
--- test/test_double_link.py
+++ test/test_double_link.py
@@ -1,6 +1,7 @@
 import unittest
 
 from data_structures.linked_list.DoubleLinkList import DoubleLinkList
+from data_structures.linked_list.Node import Node
 
 class TestDoubleLinkInit(unittest.TestCase):
     def test_init_empty_list(self):
@@ -11,9 +12,13 @@ class TestDoubleLinkInit(unittest.TestCase):
         list = DoubleLinkList("bob")
         self.assertEqual(list.head, "bob")
 
-# class TestDoubleLinkAdd:
-#     def test_something():
-#         print('hmm')
+class TestDoubleLinkAdd(unittest.TestCase):
+    def test_add_node_to_empty_list(self):
+        list = DoubleLinkList()
+        first_node = Node(50)
+
+        list.add(first_node)
+        self.assertEqual(list.head, first_node, 'first node in list not properly added')
 
 if __name__ == '__main__':
     unittest.main()
