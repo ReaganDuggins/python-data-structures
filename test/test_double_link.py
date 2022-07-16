515600a2f741bcad08d6e2f277b5718e808500f1
Reagan <7460851+ReaganDuggins@users.noreply.github.com>
2022-07-16 05:15:38 -0500

    [feature] linked list constructor takes value for list head

--- test/test_double_link.py
+++ test/test_double_link.py
@@ -2,12 +2,14 @@ import unittest
 
 from data_structures.linked_list.double_link_list import DoubleLinkList
 
-print('YO')
-
 class TestDoubleLinkInit(unittest.TestCase):
     def test_init_empty_list(self):
         list = DoubleLinkList()
         self.assertEqual(list.head, None)
+
+    def test_init_with_head(self):
+        list = DoubleLinkList("bob")
+        self.assertEqual(list.head, "bob")
 
 if __name__ == '__main__':
     unittest.main()
