c6d719d93432db184eca4d8e43562d6fcaee757e
Reagan <7460851+ReaganDuggins@users.noreply.github.com>
2022-07-16 06:09:13 -0500

    [feature] can now add nodes to front or back of list

--- data_structures/linked_list/DoubleLinkList.py
+++ data_structures/linked_list/DoubleLinkList.py
@@ -3,7 +3,7 @@ class DoubleLinkList:
         self.head = head
         self.tail = None
 
-    def add(self, new_node):
+    def add_tail(self, new_node):
         if self.head == None:
             self.head = new_node
             return
@@ -16,3 +16,18 @@ class DoubleLinkList:
         self.tail.next = new_node
         new_node.previous = self.tail
         self.tail = new_node
+
+    def add_head(self, new_node):
+        if self.head == None:
+            self.head = new_node
+            return
+        if self.tail == None:
+            self.tail = self.head
+            self.head = new_node
+            self.tail.previous = self.head
+            self.head.next = self.tail
+            return
+        
+        self.head.previous = new_node
+        new_node.next = self.head
+        self.head = new_node
--- test/test_double_link.py
+++ test/test_double_link.py
@@ -12,20 +12,20 @@ class TestDoubleLinkInit(unittest.TestCase):
         list = DoubleLinkList("bob")
         self.assertEqual(list.head, "bob")
 
-class TestDoubleLinkAdd(unittest.TestCase):
+class TestDoubleLinkAddTail(unittest.TestCase):
     def test_add_node_to_empty_list(self):
         list = DoubleLinkList()
         first_node = Node(50)
 
-        list.add(first_node)
+        list.add_tail(first_node)
         self.assertEqual(list.head, first_node, 'first node in list not properly added')
 
     def test_add_second_node_to_list(self):
         list = DoubleLinkList()
-        list.add(Node('bob'))
+        list.add_tail(Node('bob'))
 
         second_node = Node('jim')
-        list.add(second_node)
+        list.add_tail(second_node)
 
         self.assertEqual(list.head.value, 'bob', 'head was overwritten when adding second node')
         self.assertEqual(list.tail.value, 'jim', 'tail not properly set')
@@ -34,19 +34,49 @@ class TestDoubleLinkAdd(unittest.TestCase):
 
     def test_add_more_nodes_to_list(self):
         list = DoubleLinkList()
-        list.add(Node(4))
-        list.add(Node(8))
+        list.add_tail(Node(4))
+        list.add_tail(Node(8))
 
         third_node = Node(-3)
-        list.add(third_node)
+        list.add_tail(third_node)
 
         self.assertEqual(list.head.value, 4, 'head was overwritten when adding third node')
         self.assertEqual(list.tail.value, -3, 'new node not set to be tail')
         self.assertEqual(list.head.next.value, 8, 'overwrote tail instead of adding to end')
         self.assertEqual(list.tail.previous.value, 8, 'new node overwrote tail')
-
-        print(list.head.value, list.head.next.value, list.head.next.next.value, list.tail.value, list.tail.previous.value, list.tail.previous.previous.value)
         
+class TestDoubleLinkAddHead(unittest.TestCase):
+    def test_add_node_to_empty_list(self):
+        list = DoubleLinkList()
+        first_node = Node(50)
+
+        list.add_head(first_node)
+        self.assertEqual(list.head, first_node, 'first node in list not properly added')
+
+    def test_add_second_node_to_list(self):
+        list = DoubleLinkList()
+        list.add_head(Node('bob'))
+
+        second_node = Node('jim')
+        list.add_head(second_node)
+
+        self.assertEqual(list.head.value, 'jim', 'head was not overwritten when adding second node')
+        self.assertEqual(list.tail.value, 'bob', 'tail not properly set')
+        self.assertEqual(list.head.next.value, 'bob', 'head.next is not tail')
+        self.assertEqual(list.tail.previous.value, 'jim', 'tail.previoius is not head')
+
+    def test_add_more_nodes_to_list(self):
+        list = DoubleLinkList()
+        list.add_head(Node(4))
+        list.add_head(Node(8))
+
+        third_node = Node(-3)
+        list.add_head(third_node)
+
+        self.assertEqual(list.head.value, -3, 'new node not set to head')
+        self.assertEqual(list.tail.value, 4, 'tail overwritten')
+        self.assertEqual(list.head.next.value, 8, 'head not connected to tail')
+        self.assertEqual(list.tail.previous.value, 8, 'tail not connected to head')
 
 if __name__ == '__main__':
     unittest.main()
